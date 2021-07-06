4033d0a4fd903b762d38afbd6598785ead5b92f3
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-06 12:45:01 -0500

    added get methods to connect html pages

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 const PORT = process.env.PORT || 3000
 
+app.use(express.static('public'))
 
 app.get('/', (req, res) =>
-    res.send('<h1>Testing 1 2 3</h1>')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+)
+
+app.get('/stats', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public', 'stats.html'))
+)
+
+app.get('/exercise', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public', 'exercise.html'))
 )
 
 
