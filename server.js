4844b94ed803ea59af38a5fa27487143211f12dd
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-08 12:41:09 -0500

    fixed the logger import issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const path = require('path')
-const morgan = require('morgan')
+const logger = require('morgan')
 const db = require('./models')
 
 const PORT = process.env.PORT || 3001
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 app.use(express.static('public'))
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout_tracker', { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true, useCreateIndex: true})
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true, useCreateIndex: true})
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 )
