bd571fa963357f1066fbaa01f3e3be2bcd793215
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-08 17:13:44 -0500

    added comments and a new get function

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,26 +1,42 @@
 const router = require('express').Router()
 const { Exercise, Workout }  = require('../models')
 
+
+
+// posts the users choices to the datbase
 router.post('/api/workouts', ({body}, res) => {
     Workout.create(body)
-    .then(dbWorkout => {
-        res.json(dbWorkout)
-    })
-    .catch(error => {
-        console.log(error)
-    })
-})
-
-router.get('/api/workouts', (req,res) => {
-    Workout.aggregate([{
-        $addFields: {
-            totalDuration: {$sum: '$exercises.duration'},
-            totalWeight: {$sum: '$exercises.weight'}
-        }
-    }])
     .then(dbWorkout => {res.json(dbWorkout)})
     .catch(error => {console.log(error)})
 })
 
 
+// gets all of the users workouts
+router.get('/api/workouts', (req, res) => {
+    Workout.aggregate([{
+        $addFields: { totalDuration: {$sum: '$exercises.duration'}, totalWeight: {$sum: '$exercises.weight'} }}])
+    .then(dbWorkout => {res.json(dbWorkout)})
+    .catch(error => {console.log(error)})
+})
+
+
+// updates the users workouts
+router.put('/api/Workouts/:id', (req, res) => {
+    Workout.findOneAndUpdate({ _id: req.params.id }, { $push: { exercises: req.body }})
+    .then(dbWorkout => {res.json(dbWorkout)})
+    .catch(error => {console.log(`${error} testing put`)})
+})
+
+
+// takes all of the users workouts and adds them together to show their over all progress
+router.get('/api/workouts/range', (req, res) => {
+    Workout.aggregate([{
+        $addFields: { totalDuration: {$sum: '$exercises.duration'}, totalWeight: {$sum: '$exercises.weight'} }}])
+    .then(dbWorkout => {res.json(dbWorkout)})
+    .catch( error => {consol.log(error)})
+})
+
+
+
+// exports the router
 module.exports = router
--- routes/html_routes.js
+++ routes/html_routes.js
@@ -3,19 +3,24 @@ const path = require('path')
 
 
 
+// sends the index to be displayed on the home page
 router.get('/', (req, res) =>
-    res.sendFile(`${__dirname}/../public/index.html`)
+res.sendFile(`${__dirname}/../public/index.html`)
 )
 
 
+// sends the exercise file to be displayed on the exercise page
 router.get('/exercise', (req, res) =>
-    res.sendFile(path.join(`${__dirname}/../public/exercise.html`))
+res.sendFile(path.join(`${__dirname}/../public/exercise.html`))
 )
 
 
+// sends the stats file to be displayed on the home dashboard page
 router.get('/stats', (req, res) =>
     res.sendFile(path.join(`${__dirname}/../public/stats.html`))
 )
 
 
+
+// exports the the router
 module.exports = router
