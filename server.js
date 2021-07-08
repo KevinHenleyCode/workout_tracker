ae9db898a35e3d95be714b94fb9aada1db650d05
Kevin Henley <75505087+KevinHenleyCode@users.noreply.github.com>
2021-07-08 14:01:47 -0500

    added routes to clean up the script

--- server.js
+++ server.js
@@ -12,21 +12,10 @@ app.use(logger('dev'))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 app.use(express.static('public'))
+app.use(require('./routes/api_routes'))
+app.use(require('./routes/html_routes'))
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true, useCreateIndex: true})
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-)
-
-app.get('/stats', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public', 'stats.html'))
-)
-
-app.get('/exercise', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public', 'exercise.html'))
-)
-
-
 
 
 app.listen(PORT, () => console.log(`Server has started on Port: ${PORT}`))
